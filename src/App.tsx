ac530548b0d0762b59461790a185d990f4dbd14b
Renan Santos <renan-bs@hotmail.com>
2024-06-11 11:40:43 -0300

    Iniciando App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+      </div>
+    </>
+  )
+}
